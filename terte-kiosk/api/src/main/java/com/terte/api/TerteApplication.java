f52401adda879ff047733dc4728aa5652aa00227
seunghaen <tmdgus9701@naver.com>
2024-12-03 18:28:45 +0900

    issue1-project start

---
+++ terte-kiosk/api/src/main/java/com/terte/api/TerteApplication.java
@@ -0,0 +1,13 @@
+// Application.java (애플리케이션 진입점)
+package com.terte.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TerteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TerteApplication.class, args);
+    }
+}
