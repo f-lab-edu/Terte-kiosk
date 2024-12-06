a19dc23d248ed34c8d826976a5455be1c1fd2c21
seunghaen <tmdgus9701@naver.com>
2024-12-06 15:39:33 +0900

    프로젝트 구조 싱글모듈로 변경

--- terte-kiosk/src/main/java/TerteMainApplication.java
+++ terte-kiosk/src/main/java/TerteMainApplication.java
@@ -1,13 +1,10 @@
-// Application.java (애플리케이션 진입점)
-package com.terte.api;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TerteApplication {
-
+public class TerteMainApplication {
     public static void main(String[] args) {
-        SpringApplication.run(TerteApplication.class, args);
+        SpringApplication.run(TerteMainApplication.class, args);
     }
 }
+
