df84c497197d5fec68158d19f5b5d0c218157249
seunghaen <tmdgus9701@naver.com>
2025-01-06 21:13:29 +0900

    docker mysql 기본설정

--- application/src/main/java/com/terte/entity/menu/Choice.java
+++ application/src/main/java/com/terte/entity/menu/Choice.java
@@ -1,13 +1,20 @@
 package com.terte.entity.menu;
 
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.*;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
+@Entity
 public class Choice {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private Integer price;
