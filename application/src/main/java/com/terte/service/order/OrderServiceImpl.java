56c282d93145e8f69944a34a3e565c3b66e8e1ea
seunghaen <tmdgus9701@naver.com>
2025-01-31 19:56:12 +0900

    통합테스트 실패케이스 수정

--- application/src/main/java/com/terte/service/order/OrderServiceImpl.java
+++ application/src/main/java/com/terte/service/order/OrderServiceImpl.java
@@ -75,8 +75,9 @@ public class OrderServiceImpl implements OrderService {
                 for(MenuOption option: menu.getMenuOptions()){
                     if(option.getId().equals(selectedOption.getMenuOptionId())){
                         if(!option.getMultipleSelection()){
-                            long count = item.getSelectedOptions().size();
-                            if(count > 1){
+                            long count = item.getSelectedOptions().stream()
+                                    .filter(selectedOption1 -> selectedOption1.getSelectedChoiceIds().size() > 1).count();
+                            if(count > 0){
                                 throw new IllegalArgumentException(
                                         String.format("Multiple selection not allowed for option '%s' in menu '%s'", option.getName(), menu.getName())
                                 );
