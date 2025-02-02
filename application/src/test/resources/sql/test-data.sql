b65243a8b70ed5e526f64a9f1f22fc5a1e78f5f3
seunghaen <tmdgus9701@naver.com>
2025-02-02 16:38:23 +0900

    mysql gitHub action service로 수정 및 table 생성 쿼리

--- application/src/test/resources/sql/test-data.sql
+++ application/src/test/resources/sql/test-data.sql
@@ -1,4 +1,110 @@
+-- 테이블 생성
+CREATE TABLE category (
+                          id BIGINT NOT NULL AUTO_INCREMENT,
+                          description VARCHAR(255),
+                          name VARCHAR(255) NOT NULL,
+                          store_id BIGINT NOT NULL,
+                          PRIMARY KEY (id)
+) ENGINE=InnoDB;
 
+CREATE TABLE choice (
+                        id BIGINT NOT NULL AUTO_INCREMENT,
+                        name VARCHAR(255) NOT NULL,
+                        price INT NOT NULL,
+                        menu_option_id BIGINT,
+                        PRIMARY KEY (id)
+) ENGINE=InnoDB;
+
+CREATE TABLE menu (
+                      id BIGINT NOT NULL AUTO_INCREMENT,
+                      description VARCHAR(255),
+                      image VARCHAR(255),
+                      name VARCHAR(255),
+                      price INT,
+                      store_id BIGINT,
+                      category_id BIGINT,
+                      PRIMARY KEY (id)
+) ENGINE=InnoDB;
+
+CREATE TABLE menu_option (
+                             id BIGINT NOT NULL AUTO_INCREMENT,
+                             multiple_selection BIT NOT NULL,
+                             name VARCHAR(255) NOT NULL,
+                             required BIT NOT NULL,
+                             menu_id BIGINT,
+                             PRIMARY KEY (id)
+) ENGINE=InnoDB;
+
+CREATE TABLE orders (
+                        id BIGINT NOT NULL AUTO_INCREMENT,
+                        create_time DATETIME(6),
+                        order_type VARCHAR(255) NOT NULL,
+                        phone_number VARCHAR(255),
+                        status VARCHAR(255) NOT NULL,
+                        store_id BIGINT NOT NULL,
+                        table_number INT,
+                        total_price BIGINT,
+                        PRIMARY KEY (id)
+) ENGINE=InnoDB;
+
+CREATE TABLE order_item (
+                            id BIGINT NOT NULL AUTO_INCREMENT,
+                            menu_id BIGINT,
+                            quantity INT NOT NULL,
+                            order_id BIGINT,
+                            PRIMARY KEY (id)
+) ENGINE=InnoDB;
+
+CREATE TABLE payment (
+                         id BIGINT NOT NULL AUTO_INCREMENT,
+                         order_id BIGINT,
+                         payment_method VARCHAR(255),
+                         status VARCHAR(255),
+                         store_id BIGINT,
+                         total_price BIGINT,
+                         PRIMARY KEY (id)
+) ENGINE=InnoDB;
+
+CREATE TABLE selected_option (
+                                 id BIGINT NOT NULL AUTO_INCREMENT,
+                                 menu_option_id BIGINT,
+                                 order_item_id BIGINT,
+                                 PRIMARY KEY (id)
+) ENGINE=InnoDB;
+
+CREATE TABLE selected_option_choice_ids (
+                                            selected_option_id BIGINT NOT NULL,
+                                            selected_choice_ids BIGINT,
+                                            PRIMARY KEY (selected_option_id, selected_choice_ids)
+) ENGINE=InnoDB;
+
+-- 외래키 설정
+ALTER TABLE choice
+    ADD CONSTRAINT FK_choice_menu_option
+        FOREIGN KEY (menu_option_id) REFERENCES menu_option (id);
+
+ALTER TABLE menu
+    ADD CONSTRAINT FK_menu_category
+        FOREIGN KEY (category_id) REFERENCES category (id);
+
+ALTER TABLE menu_option
+    ADD CONSTRAINT FK_menu_option_menu
+        FOREIGN KEY (menu_id) REFERENCES menu (id);
+
+ALTER TABLE order_item
+    ADD CONSTRAINT FK_order_item_orders
+        FOREIGN KEY (order_id) REFERENCES orders (id);
+
+ALTER TABLE selected_option_choice_ids
+    ADD CONSTRAINT FK_selected_option_choice
+        FOREIGN KEY (selected_option_id) REFERENCES selected_option (id);
+
+ALTER TABLE selected_option
+    ADD CONSTRAINT FK_selected_option_order_item
+        FOREIGN KEY (order_item_id) REFERENCES order_item (id);
+
+
+--테스트 데이터 설정
 INSERT INTO category (id, name, store_id, description) VALUES
                                                            (1, 'COFFEE', 101, '커피 메뉴'),
                                                            (2, '빙수', 101, '맛있는 빙수'),
