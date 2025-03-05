ac361388263c887b50bd283a6404f9d67e2d7cc2
seunghaen <tmdgus9701@naver.com>
2025-03-05 19:52:27 +0900

    import 에러 수정

--- application/src/main/java/com/terte/controller/menu/PopularMenuController.java
+++ application/src/main/java/com/terte/controller/menu/PopularMenuController.java
@@ -1,5 +1,6 @@
 package com.terte.controller.menu;
 
+import com.terte.service.menu.PopularMenuService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
